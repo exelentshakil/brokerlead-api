8c28899094f2c21e6f28eea959974eb68cb9d31f
Shakil Ahmed <shakil@Shakils-MacBook-Pro.local>
2021-08-27 15:26:17 +0600

    Initial commit

---
+++ database/migrations/2021_08_27_091849_create_properties_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->nullable();
+            $table->string('address')->nullable();
+            $table->string('price')->nullable();
+            $table->string('bed')->nullable();
+            $table->string('bathroom')->nullable();
+            $table->string('year_built')->nullable();
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
